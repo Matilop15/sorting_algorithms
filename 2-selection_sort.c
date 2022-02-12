2332df43cdd1bdefc064eaf06d8e3cd8b24cf613
Marcosozhol <3956@holbertonschool.com>
2022-02-12 12:49:58 -0800

    Task 2 - method selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,34 @@
 #include "sort.h"
 /**
- *
- *
- *
+ *selection_sort - sorts an array of integers in ascending order
+ *@array: array to modify
+ *@size: size of array
  *
  */
 void selection_sort(int *array, size_t size)
 {
+	int aux = 0, i = 0, j = 0, menor = 0, ssize = size;
+
 	if (!array || !size)
 		return;
 
+	for (i = 0; i < (ssize - 1); i++)
+	{
+		menor = i;
+
+		for (j = i + 1; j < ssize; j++)
+		{
+			if (array[j] < array[menor])
+			{
+				menor = j;
+			}
+		}
+		if (array[i] > array[menor])
+		{
+			aux = array[i];
+			array[i] = array[menor];
+			array[menor] = aux;
+			print_array(array, size);
+		}
+	}
 }
